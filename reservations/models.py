ebfaa3a4b29bd363ed13f8e684b598aad2e4c084
Eoin Murphy <murphy.eoin93@gmail.com>
2023-02-26 16:21:46 +0000

    added reservations app

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class reservations(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    number_of_peopole = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.name
--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import reservations
+
+from reservations.models import reservations
 
 # Create your views here.
+
+
+def reservations(request):
+    return render(request, 'reservations.html')

ee145d97f5853dfbc92cbe79d90f4dff8c4d3065
Eoin Murphy <murphy.eoin93@gmail.com>
2023-02-28 23:20:37 +0000

    deploymentP2

--- goodtaco/settings.py
+++ goodtaco/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+bgloa4iteqio_(4ih-4hv3+^(y_*=pv96e-(9s%%5(g67zf$%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['good-taco.herokuapp.com', 'localhost']
 
